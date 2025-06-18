45ccd9fac25ad1fbf32d73c942fac597295ee56f
glaisne <glaisne@outlook.com>
2025-06-17 23:34:28 -0400

    Update certificate path

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "azurerm_linux_virtual_machine" "vm-web01" {
 
     admin_ssh_key {
         username = "gene"
-        public_key = file("${path.module}/../common/id_ed25519.pub")
+        public_key = file("${path.module}/certificate/certificate.pub")
     }
 
     os_disk {
